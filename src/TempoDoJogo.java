247a8b6f066bee2d8fa97edd38e692109a6b9da8
Victor Hugo <vhaporfiro@gmail.com>
2025-01-03 13:04:24 -0300

    Tempo do Jogo

---
+++ src/TempoDoJogo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TempoDoJogo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int horaInicial = sc.nextInt();
+        int horaFinal = sc.nextInt();
+
+        int duracao;
+
+         if (horaInicial < horaFinal) {
+            duracao = horaFinal - horaInicial;
+        } else if (horaInicial > horaFinal) {
+            duracao = (24 - horaInicial) + horaFinal;
+        } else {
+            duracao = 24;
+        }
+
+        System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+
+        sc.close();
+    }
+}
