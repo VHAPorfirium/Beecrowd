36501019d39ec1ce509d802e3d09e8987190b315
Victor Hugo <vhaporfiro@gmail.com>
2025-01-05 06:21:49 -0300

    Numeros Pares

---
+++ src/NumerosPares.java
@@ -0,0 +1,11 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+
+        for (int x = 2; x <= 100; x++) {
+            if (x % 2 == 0) {
+                System.out.println(x);
+            }
+        }
+
+    }
+}
